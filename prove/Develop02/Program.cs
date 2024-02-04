265c79f3ed79741810da38ce8cc68560f1494fb9
IamJon6275 <wel15010@byui.edu>
2024-02-03 22:34:56 -0700

    journal program finished

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // _selector is created to represent what the journal will do whe it's passed to the journal object
+        int _selector;
+
+        // _continue is set to true so the while loop can run once
+        bool _continue = true;
+        
+        Journal j = new Journal();
+        
+        // Greet the user
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        // While loop to call the different classes and loop the program until the user decides to quit
+        while (_continue)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            
+            _selector = int.Parse(Console.ReadLine());
+
+            
+            _continue = j.HandleJournal(_selector);
+        }
     }
 }
